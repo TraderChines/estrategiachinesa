d85128580a103cfa71b596b24e91fea8b2a7e880
Trader Chines <traderchinesvip@gmail.com>
2025-09-01 08:05:24 +0000

    preciso que você suba as novas mudanças para o repositorio do github htt

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type {NextConfig} from 'next';
 
+const isProd = process.env.NODE_ENV === 'production';
+
 const nextConfig: NextConfig = {
   output: 'export',
+  // assetPrefix and basePath are needed for GitHub Pages deployment
+  assetPrefix: isProd ? '/estrategiachinesa/' : undefined,
+  basePath: isProd ? '/estrategiachinesa' : undefined,
   reactStrictMode: false,
   typescript: {
     ignoreBuildErrors: true,
